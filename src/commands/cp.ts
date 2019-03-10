37d09439ff2cf9d59b1f18e99cf314232b55de48
DKFN <valere.tetelin@gmail.com>
2019-03-10 02:41:57 +0100

    Added cp options

--- src/commands/cp.ts
+++ src/commands/cp.ts
@@ -1,6 +1,7 @@
 import {Command, flags} from '@oclif/command'
 import {Config} from "../Config";
 import {Client} from "../BunnyClient";
+import * as fs from "fs";
 
 export default class Cp extends Command {
 
@@ -17,9 +18,10 @@ export default class Cp extends Command {
     storage: flags.string({char: 's'}),
     from: flags.string({char: 'f'}),
     to: flags.string({char: 't'}),
+    R: flags.string({char: 'R'}),
   };
 
-  static args = [{name: "Cp command for storages", storage: "storage"}];
+  static args = [{name: "", storage: "storage"}];
 
   async run() {
     Config.loadConfig();
@@ -30,7 +32,27 @@ export default class Cp extends Command {
       this.exit(127);
     }
 
+    if (flags.to && flags.from) {
+      if (fs.existsSync(flags.from)) {
+          if (flags.R) {
+            console.log("Not implemented");
+          } else {
+            const fd = fs.openSync(flags.from, 'r');
+            const content = fs.readFileSync(fd);
+              /*if (err) {
+                console.error(`[${err.errno}] Unable to open host file : ${err.message} ( ${err.syscall} ${err.path}`);
+                this.exit(127);
+              }*/
+              Client.uploadFile(flags.storage, content, flags.to!);
+            }
+      } else {
+          // TODO : Then it might be a download
+      }
+    }
+
     const storageZone = flags.storage;
     console.log(storageZone);
+
+    // TODO : Try to resolve path from filesystem, if unable assume its a download and storage path is first
   }
 }
