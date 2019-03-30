182764c6c90ed50450d692cd77761d479a6f2c5e
DKFN <valere.tetelin@gmail.com>
2019-03-30 17:01:39 +0100

    Type is not explicityly required in key command anymore

--- src/commands/key.ts
+++ src/commands/key.ts
@@ -3,33 +3,46 @@ import {Config, IStoredKey} from "../Config";
 
 export default class Key extends Command {
 
-  static description = 'To add / delete / set a key for a pullzon or a storage';
+  static description = 'To add / delete / set a key for account pullzones or a storage';
 
   static examples = [
     `
-    ➜  bunnycdn-cli git:(master) ✗ bnycdn key -l  
-==== PullZones : 
-Key Name        : Key Value
-default   | .....
-==== Storages: 
-Key Name        : Key Value
-default | .....
-name | .....
+        ➜  bunnycdn-cli git:(master) ✗ bnycdn key -l  
+        ==== PullZones : 
+        Key Name        : Key Value
+        default   | .....
+        ==== Storages: 
+        Key Name        : Key Value
+        default | .....
+        name | .....
 
-➜  bunnycdn-cli git:(master) ✗ bnycdn key -s myneykey -t storages -v my_api_key_from_panel
-{ k: 'myneykey', v: 'my_api_key_from_panel', t: 'storages' }
-ⓘKey successfully set: myneykey
+        Add default API Key:
+        ➜  bunnycdn-cli git:(master) ✗ bnycdn key -s default -v my_api_key_from_panel
+        ⓘ Successfully deleted key : default
 
-➜  bunnycdn-cli git:(master) ✗ bnycdn key -d myneykey -t storages                         
-ⓘSuccessfully deleted key : myneykey
+        Add aliased API Key (If you have multiple accounts):
+        ➜  bunnycdn-cli git:(master) ✗ bnycdn key -s myKeyName -v my_api_key_from_panel
+        ⓘ Successfully deleted key : myKeyName
+
+        Add a storage Key:
+        ➜  bunnycdn-cli git:(master) ✗ bnycdn key -s mynewkey -t storages -v my_storage_ftp_password
+        ⓘ Key successfully set: mynewkey
+
+        Add a default storage Key:
+        ➜  bunnycdn-cli git:(master) ✗ bnycdn key -s default -t storages -v my_storage_ftp_password
+        ⓘ Key successfully set: mynewkey
+
+        Delete a key
+        ➜  bunnycdn-cli git:(master) ✗ bnycdn key -d mynewkey -t storages                         
+        ⓘ Successfully deleted key : mynewkey
     `,
   ];
 
   static flags = {
     help: flags.help({char: 'h'}),
     // flag with a value (-n, --name=VALUE)
-    type: flags.string({char: 't', description: 'Type of the key'}),
-    set: flags.string({char: 's', description: 'Sets a key with given name'}),
+    type: flags.string({char: 't', description: 'Type of the key ( pullzones OR storages )'}),
+    set: flags.string({char: 's', description: 'Sets a key with given name '}),
     del: flags.string({char: 'd', description: 'Deletes a key with given name'}),
     value: flags.string({char: 'v', description: 'Gives a value for add and set operations'}),
     list: flags.boolean({char: 'l', description: 'lists all keys stored and their names'}),
@@ -49,15 +62,14 @@ name | .....
 
     if (flags.set) {
       // TODO : Allow edition in prompt mode if no params is specified
-      if (flags.value && flags.type && this.checkType(flags.type)) {
-        this.addKey(flags.set, flags.value, flags.type);
+      if (flags.value) {
+        this.addKey(flags.value, flags.set, flags.type);
         this.exit(0);
       } else {
         this.error(" 1 You must specify a value and a type for the set operation and must be correctly set",
           {code: "NOVALUE", exit: 1}
         );
       }
-      // TODO : Check fd ret codes
     }
 
     if (flags.del) {
@@ -89,7 +101,7 @@ name | .....
   }
 
   // TODO : Add prompt version  if parameters are unspecified
-  private addKey(k: string, v: string, t: string = "pullzones") {
+  private addKey(v: string, k?: string = "default", t: string = "pullzones") {
     Config.mergeToConf( {name: k, value: v}, t);
     Config.persistConf();
     this.log(" ⓘ Key successfully set: " + k);
