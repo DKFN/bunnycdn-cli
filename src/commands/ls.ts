b76dd96829c456c6298c1c3e686504a97024e14b
DKFN <valere.tetelin@gmail.com>
2019-05-01 13:00:56 +0200

    Added default storage key in ls command

--- src/commands/ls.ts
+++ src/commands/ls.ts
@@ -8,8 +8,21 @@ export default class Ls extends Command {
   static description = 'describe the command here';
 
   static examples = [
-    `$ bnycdn pz -l
-    Lists all the pul zone
+    `
+    $ bnycdn ls /teststorage/sample/ -s mystoragename
+    type       lastChanged              size       path                                         
+    ---------  -----------------------  ---------  ---------------------------------------------
+    [ DIR  ]   2019-04-13T13:20:45.874  0 B        /testcdn/sample/finalUpTest/node_modules  
+    [ DIR  ]   2019-04-13T13:21:03.133  0 B        /testcdn/sample/finalUpTest/.idea         
+    [ DIR  ]   2019-04-13T13:21:03.329  0 B        /testcdn/sample/finalUpTest/.git          
+    [ FILE ]   2019-04-13T13:20:52.59   1.89 MB    /testcdn/sample/finalUpTest/samplesml.mkv 
+    [ FILE ]   2019-04-13T13:21:50.066  18.82 MB   /testcdn/sample/finalUpTest/samplemid1.mkv
+    [ FILE ]   2019-04-13T13:23:57.191  56.45 MB   /testcdn/sample/finalUpTest/samplebig.mkv 
+    [ FILE ]   2019-04-13T13:20:45.876  173.59 KB  /testcdn/sample/finalUpTest/paris.jpg     
+    [ FILE ]   2019-04-13T13:21:04.011  171 B      /testcdn/sample/finalUpTest/.editorconfig 
+
+    You can also still use 0.2.x style
+    $ bnycdn ls -d /teststorage/mydirectory/ -s mystoragename
     `,
   ];
 
@@ -24,14 +37,14 @@ export default class Ls extends Command {
   async run() {
     Config.loadConfig();
     const {flags, argv} = this.parse(Ls);
+    const storage = flags.storage || "default";
     const maybeDir = argv[0] || flags.dir;
-    console.log(maybeDir);
 
-    if (!flags.storage || !maybeDir) {
+    if (!maybeDir) {
       this.error("You must specify a storage zone with -s and a flag directory with -d");
       this.exit(127);
     } else {
-      const dirContent = await Client.listDirectory(flags.storage!, maybeDir);
+      const dirContent = await Client.listDirectory(storage, maybeDir);
 
       const finalData = dirContent && dirContent
         .sort((a, b) => a.isDir && !b.isDir && -1 || 1)
